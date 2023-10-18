8bc1be2265583cd3cf022dd9f5630d4639f04879
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-10-18 14:36:24 -0400

    added plotting of pdfs

--- generate_pdfs.m
+++ generate_pdfs.m
@@ -8,5 +8,6 @@ gPDF=Flatten[Table[{x,Q,pdfFunction[5,0,x,Q]},{x,0.01,1.0,(1.0-0.01)/200},{Q,5,5
 Export[NotebookDirectory[]<>"pdf_data/gPDF.csv",gPDF]
 (* up and down quark PDFs: from WW-SIDIS *)
 uPDF=Flatten[Table[{x,Q,f1u[x,Q^2]},{x,0.01,1.0,(1.0-0.01)/200},{Q,5,50,(50.0-5.0)/200}],1]
-dPDF=Flatten[Table[{x,Q,f1u[x,Q^2]},{x,0.01,1.0,(1.0-0.01)/200},{Q,5,50,(50.0-5.0)/200}],1]
+dPDF=Flatten[Table[{x,Q,f1d[x,Q^2]},{x,0.01,1.0,(1.0-0.01)/200},{Q,5,50,(50.0-5.0)/200}],1]
+Export[NotebookDirectory[]<>"pdf_data/uPDF.csv",uPDF]
 Export[NotebookDirectory[]<>"pdf_data/dPDF.csv",dPDF]
--- pdfs.py
+++ pdfs.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from scipy.interpolate import RegularGridInterpolator
+import matplotlib.pyplot as plt
 
 # read PDF csv files from the data folder
 df_u = pd.read_csv('pdf_data/uPDF.csv')
@@ -14,23 +16,41 @@ df_g.columns = ['x', 'Q', 'f']
 #print(df_u.shape)
 #print(df_u.head)
 
-def interpolate_function(df_u):
+def interpolate_function(df):
     # Extract and sort the unique x and Q values, because RegularGridInterpolator needs them to be in increasing order
-    x_values = sorted(df_u['x'].unique())
-    Q_values = sorted(df_u['Q'].unique())
+    x_values = sorted(df['x'].unique())
+    Q_values = sorted(df['Q'].unique())
     
     # Create a matrix of function values using reindexed dataframe
     # this reindexes the dataframe so that its index is a multi-level index of the x and Q values
     # then it unstacks the dataframe, which creates a matrix of function values
-    f_values = df_u.set_index(['x', 'Q']).reindex(index=pd.MultiIndex.from_product([x_values, Q_values])).unstack().values
+    f_values = df.set_index(['x', 'Q']).reindex(index=pd.MultiIndex.from_product([x_values, Q_values])).unstack().values
     
     # Create an interpolating function
     interpolator = RegularGridInterpolator((x_values, Q_values), f_values, method='linear')
     
     return interpolator
 
-# Create an interpolating function from the DataFrame
-f_interp = interpolate_function(df_u)
+# Create interpolating functions from the dataframes
+def uPDF(x, Q):
+    f_interp = interpolate_function(df_u)
+    return f_interp([[x, Q]])[0]
 
-x, Q = 0.1, 25
-print(f_interp([[x, Q]]))  # Interpolated value at x=1.5, Q=15
+def dPDF(x, Q):
+    f_interp = interpolate_function(df_d)
+    return f_interp([[x, Q]])[0]
+
+def gPDF(x, Q):
+    f_interp = interpolate_function(df_g)
+    return f_interp([[x, Q]])[0]
+
+x_range = np.linspace(0.01, 0.99, 100)
+y1 = [x*uPDF(x, 10) for x in x_range]
+y2 = [x*dPDF(x, 10) for x in x_range]
+y3 = [x*gPDF(x, 10)/10.0 for x in x_range]
+
+plt.semilogx(x_range, y1, label='u')
+plt.semilogx(x_range, y2, label='d')
+plt.semilogx(x_range, y3, label='g/10')
+plt.legend()
+plt.show()
