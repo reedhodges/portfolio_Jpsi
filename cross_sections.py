db05c9e2766edd5732bf56cf07631d82141f02bc
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-10-23 12:36:15 -0400

    put the cross sections into a class

--- cross_sections.py
+++ cross_sections.py
@@ -18,75 +18,122 @@ def gaussian_z(z):
     avz = 0.04
     return np.exp(-(1-z)**2/avz) / np.sqrt(pi*avz)
 
-# quark fragmentation: unpolarized J/psi
-def d_sigma_FF_U(x, z, Q, PT):
-    y = Q**2/(x*s)
-    const = to_pb*((4*pi*alpha_em**2)/(Q**4)) * (1-y+y**2/2)
-    pdf = (2./3.)**2*u_interp((x,Q)) + (1./3.)**2*d_interp((x,Q))
-    return const*pdf*3*D1(z,PT,Q)
+# cross sections differnetial in x, z, Q, PT
+class differential_cross_section:
+    def __init__(self, s, O3s1sing, O1s0oct, O3p0oct, u_interp, d_interp, g_interp):
+        self.s = s
+        self.O3s1sing = O3s1sing
+        self.O1s0oct = O1s0oct
+        self.O3p0oct = O3p0oct
+        self.u_interp = u_interp
+        self.d_interp = d_interp
+        self.g_interp = g_interp
+        self.ff = self.FF(self)
+        self.pgf = self.PGF(self)
 
-# quark fragmentation: longitudinal J/psi
-def d_sigma_FF_L(x, z, Q, PT):
-    y = Q**2/(x*s)
-    const = to_pb*((4*pi*alpha_em**2)/(Q**4)) * (1-y+y**2/2)
-    pdf = (2./3.)**2*u_interp((x,Q)) + (1./3.)**2*d_interp((x,Q))
-    return const*pdf*(D1(z,PT,Q)-D1LL(z,PT,Q))
+    # cross sections for light quark fragmentation
+    class FF:
+        def __init__(self, parent):
+            self.parent = parent
+        
+        # unpolarized J/psi
+        def U(x, z, Q, PT):
+            y = Q**2/(x*s)
+            const = to_pb*((4*pi*alpha_em**2)/(Q**4)) * (1-y+y**2/2)
+            pdf = (2./3.)**2*u_interp((x,Q)) + (1./3.)**2*d_interp((x,Q))
+            return const*pdf*3*D1(z,PT,Q)
+        
+        # longitudinally polarized J/psi
+        def L(x, z, Q, PT):
+            y = Q**2/(x*s)
+            const = to_pb*((4*pi*alpha_em**2)/(Q**4)) * (1-y+y**2/2)
+            pdf = (2./3.)**2*u_interp((x,Q)) + (1./3.)**2*d_interp((x,Q))
+            return const*pdf*(D1(z,PT,Q)-D1LL(z,PT,Q))
 
-# photon-gluon fusion: 1s0 octet, unpolarized J/psi
-def d_sigma_PGF_1s0oct_U(x, z, Q, PT):
-    y = Q**2/(x*s)
-    zt = 2-z
-    const = to_pb*(1-y+y**2/2)*32*pi**3*z*alpha_em**2*alpha_s(30)/(9*M*Q**6*zt**3)
-    pdf = g_interp((x*zt,Q))
-    return const*pdf*gaussian_z(z)*gaussian_PT(PT)*O1s0oct
+    # cross sections for photon-gluon fusino    
+    class PGF:
+        def __init__(self, parent):
+            self.parent = parent
+            self.Oct1s0 = self.oct1s0(self)
+            self.Oct3p0 = self.oct3p0(self)
+            self.Sing3s1 = self.sing3s1(self)
 
-# photon-gluon fusion: 1s0 octet, long. polarized J/psi
-def d_sigma_PGF_1s0oct_L(x, z, Q, PT):
-    return d_sigma_PGF_1s0oct_U(x, z, Q, PT) / 3.
+        # 1s0 color octet channel
+        class oct1s0:
+            def __init__(self, parent):
+                self.parent = parent
 
-# photon-gluon fusion: 3p0 octet, unpolarized J/psi
-def d_sigma_PGF_3p0oct_U(x, z, Q, PT):
-    y = Q**2/(x*s)
-    zt = 2-z
-    ypoly = y**2*(8+z*(3*z-8)) + 8*(1-y)*zt - 2*(1-y)*z*zt**2
-    const = to_pb*64*pi**3*z*alpha_em**2*alpha_s(30)/(9*M**3*Q**6*zt**5)
-    pdf = g_interp((x*zt,Q))
-    return const*ypoly*pdf*gaussian_z(z)*gaussian_PT(PT)*O3p0oct
+            # unpolarized J/psi
+            def U(x, z, Q, PT):
+                y = Q**2/(x*s)
+                zt = 2-z
+                const = to_pb*(1-y+y**2/2)*32*pi**3*z*alpha_em**2*alpha_s(30)/(9*M*Q**6*zt**3)
+                pdf = g_interp((x*zt,Q))
+                return const*pdf*gaussian_z(z)*gaussian_PT(PT)*O1s0oct
+            
+            # longitudinally polarized J/psi
+            def L(x, z, Q, PT):
+                y = Q**2/(x*s)
+                zt = 2-z
+                const = to_pb*(1-y+y**2/2)*32*pi**3*z*alpha_em**2*alpha_s(30)/(9*M*Q**6*zt**3)
+                pdf = g_interp((x*zt,Q))
+                return const*pdf*gaussian_z(z)*gaussian_PT(PT)*O1s0oct / 3.
+            
+        # 3p0 color octet channel
+        class oct3p0:
+            def __init__(self, parent):
+                self.parent = parent
+            
+            # unpolarized J/psi
+            def U(x, z, Q, PT):
+                y = Q**2/(x*s)
+                zt = 2-z
+                ypoly = y**2*(8+z*(3*z-8)) + 8*(1-y)*zt - 2*(1-y)*z*zt**2
+                const = to_pb*64*pi**3*z*alpha_em**2*alpha_s(30)/(9*M**3*Q**6*zt**5)
+                pdf = g_interp((x*zt,Q))
+                return const*ypoly*pdf*gaussian_z(z)*gaussian_PT(PT)*O3p0oct
+            
+            # longitudinally polarized J/psi
+            def L(x, z, Q, PT):
+                y = Q**2/(x*s)
+                zt = 2-z
+                ypoly = (2-y)**2-2*z*(1-y)
+                const = to_pb*64*pi**3*z**3*alpha_em**2*alpha_s(30)/(9*M**3*Q**6*zt**5)
+                pdf = g_interp((x*zt,Q))
+                return const*ypoly*pdf*gaussian_z(z)*gaussian_PT(PT)*O3p0oct
+        
+        # 3s1 color singlet channel
+        class sing3s1:
+            def __init__(self, parent):
+                self.parent = parent
+            
+            # unpolarized J/psi
+            def U(x, z, Q, PT):
+                y = Q**2/(x*s)
+                zt = 2-z
+                zb = 1-z
+                const = to_pb*512*pi*zb*alpha_em**2*alpha_s(30)**2/(243*M*Q**6*z*zt**2)
+                PTpoly = (PT**2 + zb**2*M**2*(2-z*zt)) / ((PT**2 + zb**2*M**2)**2)
+                pdf = g_interp((x,Q))
+                return const*(1-y+y**2/2.)*pdf*PTpoly*O3s1sing
+            
+            # longitudinally polarized J/psi
+            def L(x, z, Q, PT):
+                y = Q**2/(x*s)
+                zt = 2-z
+                zb = 1-z
+                const = to_pb*512*pi*zb*alpha_em**2*alpha_s(30)**2/(243*M*Q**6*z*zt**2)
+                PTpoly = PT**2 / ((PT**2 + zb**2*M**2)**2)
+                pdf = g_interp((x,Q))
+                return const*(1-y+y**2/2.)*pdf*PTpoly*O3s1sing
 
-# photon-gluon fusion: 3p0 octet, long. polarized J/psi
-def d_sigma_PGF_3p0oct_L(x, z, Q, PT):
-    y = Q**2/(x*s)
-    zt = 2-z
-    ypoly = (2-y)**2-2*z*(1-y)
-    const = to_pb*64*pi**3*z**3*alpha_em**2*alpha_s(30)/(9*M**3*Q**6*zt**5)
-    pdf = g_interp((x*zt,Q))
-    return const*ypoly*pdf*gaussian_z(z)*gaussian_PT(PT)*O3p0oct
+d_sigma = differential_cross_section(s, O3s1sing, O1s0oct, O3p0oct, u_interp, d_interp, g_interp)
 
-# photon-gluon fusion: 3s1 singled, unpolarized J/psi
-def d_sigma_PGF_3s1sing_U(x, z, Q, PT):
-    y = Q**2/(x*s)
-    zt = 2-z
-    zb = 1-z
-    const = to_pb*512*pi*zb*alpha_em**2*alpha_s(30)**2/(243*M*Q**6*z*zt**2)
-    PTpoly = (PT**2 + zb**2*M**2*(2-z*zt)) / ((PT**2 + zb**2*M**2)**2)
-    pdf = g_interp((x,Q))
-    return const*(1-y+y**2/2.)*pdf*PTpoly*O3s1sing
-
-# photon-gluon fusion: 3s1 singled, long. polarized J/psi
-def d_sigma_PGF_3s1sing_L(x, z, Q, PT):
-    y = Q**2/(x*s)
-    zt = 2-z
-    zb = 1-z
-    const = to_pb*512*pi*zb*alpha_em**2*alpha_s(30)**2/(243*M*Q**6*z*zt**2)
-    PTpoly = PT**2 / ((PT**2 + zb**2*M**2)**2)
-    pdf = g_interp((x,Q))
-    return const*(1-y+y**2/2.)*pdf*PTpoly*O3s1sing
-
-print(d_sigma_FF_U(0.1, 0.5, 10., 1.))
-print(d_sigma_FF_L(0.1, 0.5, 10., 1.))
-print(d_sigma_PGF_1s0oct_U(0.1, 0.5, 10., 1.))
-print(d_sigma_PGF_1s0oct_L(0.1, 0.5, 10., 1.))
-print(d_sigma_PGF_3p0oct_U(0.1, 0.5, 10., 1.))
-print(d_sigma_PGF_3p0oct_L(0.1, 0.5, 10., 1.))
-print(d_sigma_PGF_3s1sing_U(0.1, 0.5, 10., 1.))
-print(d_sigma_PGF_3s1sing_L(0.1, 0.5, 10., 1.))
+print (d_sigma.FF.U(0.1, 0.5, 10., 1.))
+print (d_sigma.FF.L(0.1, 0.5, 10., 1.))
+print (d_sigma.PGF.oct1s0.U(0.1, 0.5, 10., 1.))
+print (d_sigma.PGF.oct1s0.L(0.1, 0.5, 10., 1.))
+print (d_sigma.PGF.oct3p0.U(0.1, 0.5, 10., 1.))
+print (d_sigma.PGF.oct3p0.L(0.1, 0.5, 10., 1.))
+print (d_sigma.PGF.sing3s1.U(0.1, 0.5, 10., 1.))
+print (d_sigma.PGF.sing3s1.L(0.1, 0.5, 10., 1.))
