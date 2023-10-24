534cece21f2faccbbf135e1bbb34ed70f6da2bc0
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-10-24 11:19:17 -0400

    turned percent_contributions into a class

--- main.py
+++ main.py
@@ -10,32 +10,44 @@ Q_domain = np.linspace(10., 50., 10)
 PT_domain = np.linspace(0.01, 6., 50)
 
 # functions to compute the percentage of d_sigma contributions from the various LDMEs
-def percent_O3S1OCT(x, z, Q, PT):
-    d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-    return d_sigma.FF.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
+class percent_contributions:
+    def __init__(self, S: float, O3S1SING: float, O1S0OCT: float, O3P0OCT: float, u_interp, d_interp, g_interp):
+        self.S: float = S
+        self.O3S1SING: float = O3S1SING
+        self.O1S0OCT: float = O1S0OCT
+        self.O3P0OCT: float = O3P0OCT
+        self.u_interp = u_interp
+        self.d_interp = d_interp
+        self.g_interp = g_interp
+        self.d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
 
-def percent_O1S0OCT(x, z, Q, PT):
-    d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-    return d_sigma.PGF.oct1s0.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
-
-def percent_O3P0OCT(x, z, Q, PT):
-    d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-    return d_sigma.PGF.oct3p0.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
-
-def percent_O3S1SING(x, z, Q, PT):
-    d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-    return d_sigma.PGF.sing3s1.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
-
-#print(percent_O3S1OCT(0.1, 0.5, 10., 1.))
+    # percentage of d_sigma from the 3s1 color octet channel
+    def from_O3S1OCT(self, x, z, Q, PT):
+        return self.d_sigma.FF.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
+    
+    # percentage of d_sigma from the 1s0 color octet channel
+    def from_O1S0OCT(self, x, z, Q, PT):
+        return self.d_sigma.PGF.oct1s0.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
+    
+    # percentage of d_sigma from the 3p0 color octet channel
+    def from_O3P0OCT(self, x, z, Q, PT):
+        return self.d_sigma.PGF.oct3p0.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
+    
+    # percentage of d_sigma from the 3s1 color singlet channel
+    def from_O3S1SING(self, x, z, Q, PT):
+        return self.d_sigma.PGF.sing3s1.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
 
 # generate grids
 x_grid, z_grid, Q_grid, PT_grid = np.meshgrid(x_domain, z_domain, Q_domain, PT_domain)
 
+# initialize percentage class instance
+percent = percent_contributions(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
+
 # compute percentage of d_sigma contributions on the grid
-percent_O3S1OCT_grid = percent_O3S1OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-percent_O1S0OCT_grid = percent_O1S0OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-percent_O3P0OCT_grid = percent_O3P0OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-percent_O3S1SING_grid = percent_O3S1SING(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
+percent_O3S1OCT_grid = percent.from_O3S1OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
+percent_O1S0OCT_grid = percent.from_O1S0OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
+percent_O3P0OCT_grid = percent.from_O3P0OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
+percent_O3S1SING_grid = percent.from_O3S1SING(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
 total_grid = percent_O3S1OCT_grid + percent_O1S0OCT_grid + percent_O3P0OCT_grid + percent_O3S1SING_grid
 
 # create a pandas dataframe from the grid
@@ -51,6 +63,6 @@ df = pd.DataFrame({
     'total': total_grid
     })
 
-#print(df.head)
-#print(df.describe())
+print(df.head)
+print(df.describe())
 
