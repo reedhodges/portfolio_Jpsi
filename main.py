22a7888292e29dd7b3c8e633305cb06a855f1bc4
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-10-25 10:23:29 -0400

    testing dbscan

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+
+# MAYBE USE PREPROCESSING TO SCALE THE VARIABLES INSTEAD OF IN PANDAS
 
 # import /cs_data/percent_contributions.csv
 df = pd.read_csv('cs_data/percent_contributions.csv')
@@ -37,3 +41,15 @@ importances = rf.feature_importances_
 features = X.columns
 for feature, importance in zip(features, importances):
     print(f"Importance of {feature}: {importance}")
+
+#dbscan = DBSCAN(eps=0.3, min_samples=5)
+#labels = dbscan.fit_predict(X_train)
+
+#plt.scatter(X_train[:, 0], X_train[:, 1], c=labels, cmap='viridis', edgecolor='k', s=100)
+#plt.xlabel('Feature x')
+#plt.ylabel('Feature z')
+#plt.title('DBSCAN Clustering')
+#plt.show()
+
+
+# SHOULD CONSIDER FEATURE ENGINEERING WITH PRODUCTS/RATIOS OF FEATURES
