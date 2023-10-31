2653f7ed51409649b2d4c2749a74ade11aad292a
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-10-31 11:30:32 -0400

    Update R_analysis.r

--- R_analysis.r
+++ R_analysis.r
@@ -1,13 +1,26 @@
 data <- read.csv("cs_data/percent_contributions.csv")
 head(data)
-threshold <- quantile(data$percent_O3S1OCT, 0.99)
-large_values <- data[data$percent_O3S1OCT > threshold, ]
-subdomain_x <- range(large_values$x)
-subdomain_z <- range(large_values$z)
-subdomain_Q <- range(large_values$Q)
-subdomain_PT <- range(large_values$PT)
 
-print(subdomain_x)
-print(subdomain_z)
-print(subdomain_Q)
-print(subdomain_PT)
+columns <- c("percent_O3S1OCT", "percent_O1S0OCT", "percent_O3P0OCT", "percent_O3S1SING")
+
+results <- list()
+
+for (col in columns) {
+  threshold <- quantile(data[[col]], 0.99)
+  large_values <- data[data[[col]] > threshold, ]
+
+  subdomain_x <- range(large_values$x, na.rm=TRUE)
+  subdomain_z <- range(large_values$z, na.rm=TRUE)
+  subdomain_Q <- range(large_values$Q, na.rm=TRUE)
+  subdomain_PT <- range(large_values$PT, na.rm=TRUE)
+
+  results[[col]] <- list(
+      x_range = subdomain_x,
+      z_range = subdomain_z,
+      Q_range = subdomain_Q,
+      PT_range = subdomain_PT
+  )
+  
+  print(col)
+  print(results[[col]])
+}
