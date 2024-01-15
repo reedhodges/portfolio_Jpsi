a554004559dfaaf64b6df5b92604411a0d19c697
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2024-01-15 14:00:33 -0500

    improved code readability, removed percentage calculations

--- cross_sections.py
+++ cross_sections.py
@@ -4,9 +4,11 @@ from fragmentation_functions import D1, D1LL
 import numpy as np
 
 # Compute interpolators
-u_interp = create_interpolator(df_u)
-d_interp = create_interpolator(df_d)
-g_interp = create_interpolator(df_g)
+interpolators = {
+    'u_interp': create_interpolator(df_u),
+    'd_interp': create_interpolator(df_d),
+    'g_interp': create_interpolator(df_g)
+}
 
 # Gaussian PT distribution
 def gaussian_PT(PT):
@@ -20,14 +22,12 @@ def gaussian_z(z):
 
 # cross sections differnetial in x, z, Q, PT
 class differential_cross_section:
-    def __init__(self, S: float, O3S1SING: float, O1S0OCT: float, O3P0OCT: float, u_interp, d_interp, g_interp):
+    def __init__(self, S: float, O3S1OCT: float, O3S1SING: float, O1S0OCT: float, O3P0OCT: float):
         self.S: float = S
+        self.O3S1OCT: float = O3S1OCT
         self.O3S1SING: float = O3S1SING
         self.O1S0OCT: float = O1S0OCT
         self.O3P0OCT: float = O3P0OCT
-        self.u_interp = u_interp
-        self.d_interp = d_interp
-        self.g_interp = g_interp
         self.ff = self.FF(self)
         self.pgf = self.PGF(self)
 
@@ -40,17 +40,17 @@ class differential_cross_section:
         def U(x, z, Q, PT):
             y = Q**2/(x*S)
             const = TO_PB*((4*PI*ALPHA_EM**2)/(Q**4)) * (1-y+y**2/2)
-            pdf = (2./3.)**2*u_interp((x,Q)) + (1./3.)**2*d_interp((x,Q))
+            pdf = (2./3.)**2*interpolators['u_interp']((x,Q)) + (1./3.)**2*interpolators['d_interp']((x,Q))
             return const*pdf*3*D1(z,PT,M)
         
         # longitudinally polarized J/psi
         def L(x, z, Q, PT):
             y = Q**2/(x*S)
             const = TO_PB*((4*PI*ALPHA_EM**2)/(Q**4)) * (1-y+y**2/2)
-            pdf = (2./3.)**2*u_interp((x,Q)) + (1./3.)**2*d_interp((x,Q))
+            pdf = (2./3.)**2*interpolators['u_interp']((x,Q)) + (1./3.)**2*interpolators['d_interp']((x,Q))
             return const*pdf*(D1(z,PT,M)-D1LL(z,PT,M))
 
-    # cross sections for photon-gluon fusino    
+    # cross sections for photon-gluon fusion  
     class PGF:
         def __init__(self, parent):
             self.parent = parent
@@ -68,7 +68,7 @@ class differential_cross_section:
                 y = Q**2/(x*S)
                 zt = 2-z
                 const = TO_PB*(1-y+y**2/2)*32*PI**3*z*ALPHA_EM**2*alpha_s(30)/(9*M*Q**6*zt**3)
-                pdf = g_interp((x,Q))
+                pdf = interpolators['g_interp']((x,Q))
                 return const*pdf*gaussian_z(z)*gaussian_PT(PT)*O1S0OCT
             
             # longitudinally polarized J/psi
@@ -76,7 +76,7 @@ class differential_cross_section:
                 y = Q**2/(x*S)
                 zt = 2-z
                 const = TO_PB*(1-y+y**2/2)*32*PI**3*z*ALPHA_EM**2*alpha_s(30)/(9*M*Q**6*zt**3)
-                pdf = g_interp((x,Q))
+                pdf = interpolators['g_interp']((x,Q))
                 return const*pdf*gaussian_z(z)*gaussian_PT(PT)*O1S0OCT / 3.
             
         # 3p0 color octet channel
@@ -90,7 +90,7 @@ class differential_cross_section:
                 zt = 2-z
                 ypoly = y**2*(8+z*(3*z-8)) + 8*(1-y)*zt - 2*(1-y)*z*zt**2
                 const = TO_PB*64*PI**3*z*ALPHA_EM**2*alpha_s(30)/(9*M**3*Q**6*zt**5)
-                pdf = g_interp((x,Q))
+                pdf = interpolators['g_interp']((x,Q))
                 return const*ypoly*pdf*gaussian_z(z)*gaussian_PT(PT)*O3P0OCT
             
             # longitudinally polarized J/psi
@@ -99,7 +99,7 @@ class differential_cross_section:
                 zt = 2-z
                 ypoly = (2-y)**2-2*z*(1-y)
                 const = TO_PB*64*PI**3*z**3*ALPHA_EM**2*alpha_s(30)/(9*M**3*Q**6*zt**5)
-                pdf = g_interp((x,Q))
+                pdf = interpolators['g_interp']((x,Q))
                 return const*ypoly*pdf*gaussian_z(z)*gaussian_PT(PT)*O3P0OCT
         
         # 3s1 color singlet channel
@@ -114,7 +114,7 @@ class differential_cross_section:
                 zb = 1-z
                 const = TO_PB*512*PI*zb*ALPHA_EM**2*alpha_s(30)**2/(243*M*Q**6*z*zt**2)
                 PTpoly = (PT**2 + zb**2*M**2*(2-z*zt)) / ((PT**2 + zb**2*M**2)**2)
-                pdf = g_interp((x,Q))
+                pdf = interpolators['g_interp']((x,Q))
                 return const*(1-y+y**2/2.)*pdf*PTpoly*O3S1SING
             
             # longitudinally polarized J/psi
@@ -124,26 +124,5 @@ class differential_cross_section:
                 zb = 1-z
                 const = TO_PB*512*PI*zb*ALPHA_EM**2*alpha_s(30)**2/(243*M*Q**6*z*zt**2)
                 PTpoly = PT**2 / ((PT**2 + zb**2*M**2)**2)
-                pdf = g_interp((x,Q))
+                pdf = interpolators['g_interp']((x,Q))
                 return const*(1-y+y**2/2.)*pdf*PTpoly*O3S1SING
-            
-def total_sigma_U(x, z, Q, PT):
-    d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-    return d_sigma.FF.U(x, z, Q, PT) + d_sigma.PGF.oct1s0.U(x, z, Q, PT) + d_sigma.PGF.oct3p0.U(x, z, Q, PT) + d_sigma.PGF.sing3s1.U(x, z, Q, PT)
-
-def total_sigma_L(x, z, Q, PT):
-    d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-    return d_sigma.FF.L(x, z, Q, PT) + d_sigma.PGF.oct1s0.L(x, z, Q, PT) + d_sigma.PGF.oct3p0.L(x, z, Q, PT) + d_sigma.PGF.sing3s1.L(x, z, Q, PT)
-
-#print(total_sigma_U(0.1, 0.5, 10., 1.))
-
-#d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
-
-#print(d_sigma.FF.U(0.1, 0.5, 10., 1.))
-#print(d_sigma.FF.L(0.1, 0.5, 10., 1.))
-#print(d_sigma.PGF.oct1s0.U(0.1, 0.5, 10., 1.))
-#print(d_sigma.PGF.oct1s0.L(0.1, 0.5, 10., 1.))
-#print(d_sigma.PGF.oct3p0.U(0.1, 0.5, 10., 1.))
-#print(d_sigma.PGF.oct3p0.L(0.1, 0.5, 10., 1.))
-#print(d_sigma.PGF.sing3s1.U(0.1, 0.5, 10., 1.))
-#print(d_sigma.PGF.sing3s1.L(0.1, 0.5, 10., 1.))
--- generate_grids.py
+++ generate_grids.py
@@ -1,68 +1,53 @@
 import numpy as np
 import pandas as pd
-from cross_sections import differential_cross_section, total_sigma_U, u_interp, d_interp, g_interp
-from constants import S, O3S1SING, O1S0OCT, O3P0OCT
 import os
 
-# functions to compute the percentage of d_sigma contributions from the various LDMEs
-class percent_contributions:
-    def __init__(self, S: float, O3S1SING: float, O1S0OCT: float, O3P0OCT: float, u_interp, d_interp, g_interp):
-        self.S: float = S
-        self.O3S1SING: float = O3S1SING
-        self.O1S0OCT: float = O1S0OCT
-        self.O3P0OCT: float = O3P0OCT
-        self.u_interp = u_interp
-        self.d_interp = d_interp
-        self.g_interp = g_interp
-        self.d_sigma = differential_cross_section(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
+from cross_sections import differential_cross_section
+from constants import S, O3S1OCT, O3S1SING, O1S0OCT, O3P0OCT
 
-    # percentage of d_sigma from the 3s1 color octet channel
-    def from_O3S1OCT(self, x, z, Q, PT):
-        return self.d_sigma.FF.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
+d_sigma = differential_cross_section(S, O3S1OCT, O3S1SING, O1S0OCT, O3P0OCT)
     
-    # percentage of d_sigma from the 1s0 color octet channel
-    def from_O1S0OCT(self, x, z, Q, PT):
-        return self.d_sigma.PGF.oct1s0.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
-    
-    # percentage of d_sigma from the 3p0 color octet channel
-    def from_O3P0OCT(self, x, z, Q, PT):
-        return self.d_sigma.PGF.oct3p0.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
-    
-    # percentage of d_sigma from the 3s1 color singlet channel
-    def from_O3S1SING(self, x, z, Q, PT):
-        return self.d_sigma.PGF.sing3s1.U(x, z, Q, PT) / total_sigma_U(x, z, Q, PT)
-    
-# domains for the kinematic variables
-x_domain = np.linspace(0.1, 0.99, 25)
-z_domain = np.linspace(0.1, 0.8, 25)
-Q_domain = np.linspace(10., 50., 25)
-PT_domain = np.linspace(0.01, 6., 75)
+domains = {
+    'x_domain': np.linspace(0.1, 0.99, 25),
+    'z_domain': np.linspace(0.1, 0.8, 25),
+    'Q_domain': np.linspace(10., 50., 25),
+    'PT_domain': np.linspace(0.01, 6., 75)
+}
 
-# generate grids
-x_grid, z_grid, Q_grid, PT_grid = np.meshgrid(x_domain, z_domain, Q_domain, PT_domain)
+x_grid, z_grid, Q_grid, PT_grid = np.meshgrid(
+    domains['x_domain'], domains['z_domain'], domains['Q_domain'], domains['PT_domain']
+)
 
-# initialize percentage class instance
-percent = percent_contributions(S, O3S1SING, O1S0OCT, O3P0OCT, u_interp, d_interp, g_interp)
+# compute d_sigma contributions on the grid
+def compute_grid_values(d_sigma, x_grid, z_grid, Q_grid, PT_grid):
+    grid_values = {}
+    for process in ['FF', 'PGF']:
+        for pol in ['U', 'L']:
+            if process == 'FF':
+                key = f'oct3s1_{pol}_grid'
+                func = getattr(d_sigma.FF, pol)
+                grid_values[key] = func(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
+            elif process == 'PGF':
+                for ldme in ['oct1s0', 'oct3p0', 'sing3s1']:
+                    key = f'{ldme}_{pol}_grid'
+                    nested_class_instance = getattr(d_sigma.PGF, ldme)
+                    func = getattr(nested_class_instance, pol)
+                    grid_values[key] = func(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
+    return grid_values
 
-# compute percentage of d_sigma contributions on the grid
-percent_O3S1OCT_grid = percent.from_O3S1OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-percent_O1S0OCT_grid = percent.from_O1S0OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-percent_O3P0OCT_grid = percent.from_O3P0OCT(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-percent_O3S1SING_grid = percent.from_O3S1SING(x_grid.ravel(), z_grid.ravel(), Q_grid.ravel(), PT_grid.ravel())
-total_grid = percent_O3S1OCT_grid + percent_O1S0OCT_grid + percent_O3P0OCT_grid + percent_O3S1SING_grid
+grid_values = compute_grid_values(d_sigma, x_grid, z_grid, Q_grid, PT_grid)
 
-# create a pandas dataframe from the grid
-df = pd.DataFrame({
-    'x': x_grid.ravel(), 
-    'z': z_grid.ravel(), 
-    'Q': Q_grid.ravel(), 
-    'PT': PT_grid.ravel(), 
-    'percent_O3S1OCT': percent_O3S1OCT_grid, 
-    'percent_O1S0OCT': percent_O1S0OCT_grid,
-    'percent_O3P0OCT': percent_O3P0OCT_grid,
-    'percent_O3S1SING': percent_O3S1SING_grid,
-    'total': total_grid
-    })
+data = {
+    'x': x_grid.ravel(),
+    'z': z_grid.ravel(),
+    'Q': Q_grid.ravel(),
+    'PT': PT_grid.ravel()
+}
+
+for key, value in grid_values.items():
+    data[key] = value
+
+df = pd.DataFrame(data)
 
 # the proper domain for PT is actually a function of z and Q, 
 # so need to remove the ones that violate the domain.
@@ -74,36 +59,16 @@ def Q_bin(Q):
 # apply these piecewise functions to the dataframe values
 z_bin_min = df['z'].apply(lambda x: z_bin(x))
 Q_bin_min = df['Q'].apply(lambda x: Q_bin(x))
-# now define a condition on PT
+# define a condition on PT
 condition = df['PT'] <= z_bin_min * Q_bin_min / 2.
-# now remove the rows that violate the condition
+# remove the rows that violate the condition
 df = df.loc[condition]
-
-# remote outliers from the dataframe with Z-score of more than 3
-df['z_score_O3S1OCT'] = np.abs((df['percent_O3S1OCT'] - df['percent_O3S1OCT'].mean())/df['percent_O3S1OCT'].std())
-df['z_score_O1S0OCT'] = np.abs((df['percent_O1S0OCT'] - df['percent_O1S0OCT'].mean())/df['percent_O1S0OCT'].std())
-df['z_score_O3P0OCT'] = np.abs((df['percent_O3P0OCT'] - df['percent_O3P0OCT'].mean())/df['percent_O3P0OCT'].std())
-df['z_score_O3S1SING'] = np.abs((df['percent_O3S1SING'] - df['percent_O3S1SING'].mean())/df['percent_O3S1SING'].std())
-
-df = df.loc[df['z_score_O3S1OCT'] <= 3.]
-df = df.loc[df['z_score_O1S0OCT'] <= 3.]
-df = df.loc[df['z_score_O3P0OCT'] <= 3.]
-df = df.loc[df['z_score_O3S1SING'] <= 3.]
-
-# drop the z-score columns
-df = df.drop(columns=['z_score_O3S1OCT', 'z_score_O1S0OCT', 'z_score_O3P0OCT', 'z_score_O3S1SING'])
-
-# remove rows with negative percentages or percentages greater than 1
-df = df.loc[(0. <= df['percent_O3S1OCT']) & (df['percent_O3S1OCT'] <= 1.)]
-df = df.loc[(0. <= df['percent_O1S0OCT']) & (df['percent_O1S0OCT'] <= 1.)]
-df = df.loc[(0. <= df['percent_O3P0OCT']) & (df['percent_O3P0OCT'] <= 1.)]
-df = df.loc[(0. <= df['percent_O3S1SING']) & (df['percent_O3S1SING'] <= 1.)]
 
 print(df.describe())
 
 # save dataframe to csv
 cwd = os.getcwd()
 subdir = 'cs_data'
-filename = 'percent_contributions.csv'
+filename = 'cross_sections.csv'
 savepath = os.path.join(cwd, subdir, filename)
 df.to_csv(savepath, index=False)
